183bb2c2bfda2884779224ece8bc14676943f274
katieburrows <katie_burrows@me.com>
2019-04-25 22:26:34 -0600

    added rows dynamically to table

--- assets/logic.js
+++ assets/logic.js
@@ -1,6 +1,37 @@
 $(document).ready(function () {
     $("#submitBtn").on("click", function(event){
         event.preventDefault();
+        var employeeName = $("#employeeName").val().trim();
+        var role = $("#role").val().trim();
+        // var startDate = $("#startDate").val().trim();
+        var monthlyRate = $("#monthlyRate").val().trim();
+    
+        $("#employeeName").val("");
+        $("#role").val("");
+        $("#startDate").val("");
+        $("#monthlyRate").val("");
 
-    })
+
+        // $("#employeeInfo > tbody").append("<tr><td>" + employeeName + "</td><td>" +  role + "</td><td>" +
+        // empStartPretty + "</td><td>" + empMonths + "</td><td>" + monthlyRate + "</td><td>" + empBilled + "</td></tr>");
+
+   
+    });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 });
