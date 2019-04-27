cb74d6cb09f0948d8e989ac706d39fa2797226a7
katieburrows <katie_burrows@me.com>
2019-04-26 21:19:44 -0600

    added firebase

--- assets/logic.js
+++ assets/logic.js
@@ -1,4 +1,18 @@
 $(document).ready(function () {
+
+    // Initialize Firebase
+    var config = {
+        apiKey: "AIzaSyB_64GfN1BfeNiHdgHwiP55xvCwG_kAQTI",
+        authDomain: "project-1-4ebab.firebaseapp.com",
+        databaseURL: "https://project-1-4ebab.firebaseio.com",
+        projectId: "project-1-4ebab",
+        storageBucket: "project-1-4ebab.appspot.com",
+        messagingSenderId: "803889008587"
+    };
+  
+    firebase.initializeApp(config);
+
+
     $("#submitBtn").on("click", function(event){
         event.preventDefault();
         var employeeName = $("#employeeName").val().trim();
@@ -15,7 +29,7 @@ $(document).ready(function () {
         // $("#employeeInfo > tbody").append("<tr><td>" + employeeName + "</td><td>" +  role + "</td><td>" +
         // empStartPretty + "</td><td>" + empMonths + "</td><td>" + monthlyRate + "</td><td>" + empBilled + "</td></tr>");
 
-   
+        //at phase 2--push part.  look at file: 18-Push for reference
     });
 
 
