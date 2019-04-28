bde1cb9f1147637164e98df142840ee9a82fa8b7
katieburrows <katie_burrows@me.com>
2019-04-27 20:10:38 -0600

    removed firebase api key for security for time being

--- assets/logic.js
+++ assets/logic.js
@@ -1,16 +1,7 @@
 $(document).ready(function () {
 
     // Initialize Firebase
-    var config = {
-        apiKey: "AIzaSyB_64GfN1BfeNiHdgHwiP55xvCwG_kAQTI",
-        authDomain: "project-1-4ebab.firebaseapp.com",
-        databaseURL: "https://project-1-4ebab.firebaseio.com",
-        projectId: "project-1-4ebab",
-        storageBucket: "project-1-4ebab.appspot.com",
-        messagingSenderId: "803889008587"
-    };
-  
-    firebase.initializeApp(config);
+
 
     var database = firebase.database();
 
@@ -34,12 +25,21 @@ $(document).ready(function () {
         database.ref().push({
             name: employeeName,
             role: role,
-            startDate: startDate,
-            monthlyRate: monthlyRate
+            // startDate: startDate,
+            monthlyRate: monthlyRate,
+            // dateAdded: firebase.database.ServerValue.TIMESTAMP
         });
 
-        // $("#employeeInfo > tbody").append("<tr><td>" + employeeName + "</td><td>" +  role + "</td><td>" +
-        // empStartPretty + "</td><td>" + empMonths + "</td><td>" + monthlyRate + "</td><td>" + empBilled + "</td></tr>");
+        database.ref().on("child_added", function(childSnapshot){
+        $("#employeeInfo").append("<tr><td>" + childSnapshot.val().name + "</td><td>" +  childSnapshot.val().role + "</td><td>" + childSnapshot.val().monthlyRate + "</td></tr>"); 
+        // + empStartPretty + "</td><td>" + empMonths + "</td><td>" + monthlyRate + "</td><td>" + empBilled + "</td></tr>");
+
+
+        }, function(errorObject){
+            console.log("Errors handled: " + errorObject.code);
+        });
+
+
 
 
     });
